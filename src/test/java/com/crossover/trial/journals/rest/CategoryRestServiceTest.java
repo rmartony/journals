c68cb84cce78272646ca2bc48a82f1b54e735c3a
rmartony <rmartony@gmail.com>
2017-02-19 17:18:08 -0300

    CategoryRestServiceTest update

--- src/test/java/com/crossover/trial/journals/rest/CategoryRestServiceTest.java
+++ src/test/java/com/crossover/trial/journals/rest/CategoryRestServiceTest.java
@@ -1,14 +1,6 @@
 package com.crossover.trial.journals.rest;
 
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-
-import java.nio.charset.Charset;
-
+import com.crossover.trial.journals.Application;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,7 +12,12 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.crossover.trial.journals.Application;
+import java.nio.charset.Charset;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -31,28 +28,27 @@ public class CategoryRestServiceTest {
 
 	@Autowired
 	private WebApplicationContext webApplicationContext;
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
 	@Before
 	public void setup() throws Exception {
 		this.mockMvc = webAppContextSetup(webApplicationContext).build();
 	}
 
-	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
-
 	@Test
 	public void getCategories() throws Exception {
 		mockMvc.perform(get("/public/rest/category")).andExpect(status().isOk())
 				.andExpect(content().contentType(contentType))
 				.andExpect(jsonPath("$[0].id", is(1)))
-				.andExpect(jsonPath("$[0].name", is("surgery")))
-				.andExpect(jsonPath("$[1].id", is(2)))
-				.andExpect(jsonPath("$[1].name", is("ophthalmology")))
-				.andExpect(jsonPath("$[2].id", is(3)))
-				.andExpect(jsonPath("$[2].name", is("therapy")))
-				.andExpect(jsonPath("$[3].id", is(4)))
-				.andExpect(jsonPath("$[3].name", is("stomatology")))
-				.andExpect(jsonPath("$[4].id", is(5)))
-				.andExpect(jsonPath("$[4].name", is("cardiology")));
-	}
+                .andExpect(jsonPath("$[0].name", is("immunology")))
+                .andExpect(jsonPath("$[1].id", is(2)))
+                .andExpect(jsonPath("$[1].name", is("pathology")))
+                .andExpect(jsonPath("$[2].id", is(3)))
+                .andExpect(jsonPath("$[2].name", is("endocrinology")))
+                .andExpect(jsonPath("$[3].id", is(4)))
+                .andExpect(jsonPath("$[3].name", is("microbiology")))
+                .andExpect(jsonPath("$[4].id", is(5)))
+                .andExpect(jsonPath("$[4].name", is("neurology")));
+    }
 }
